a28ed4d70f6fee9862d7c807e6ebe6ad746fbfb5
Tom LE GROS <73847717+FreezeKaz@users.noreply.github.com>
2022-01-13 15:21:00 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-80px";
+  }
+}
+//carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  changeText(slideIndex = n)
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+function changeText(slide)
+{
+  document.getElementById('imgDescription').innerHTML = " 1"
+  switch (slide) {
+    case 1:
+      document.getElementById('imgDescription').innerHTML = " 1"
+      break;
+      case 2:
+        document.getElementById('imgDescription').innerHTML = " 2"
+        break;
+        case 3:
+          document.getElementById('imgDescription').innerHTML = " 3"
+          break;
+          case 4:
+            document.getElementById('imgDescription').innerHTML = " 4"
+            break;
+    default:
+
+  }
+}
